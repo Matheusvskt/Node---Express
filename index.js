23164a54d2d2cdfa9484e22d98a97cafd601f431
matheusvskt <matheusvieira99@gmail.com>
2022-01-07 15:35:06 -0300

    method get

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const path = require("path");
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'client')));
+
+app.use("/meusite",express.static(path.join(__dirname, 'client')));
+
+app.get("/",(req, res)=>{
+    //res.set("Content-Type", "text/html")
+    res.type("html")
+
+    res.send("<h1>Hello word from GET</h1>")
+})
 
 const PORT = 5000;
 app.listen(PORT, () => {
